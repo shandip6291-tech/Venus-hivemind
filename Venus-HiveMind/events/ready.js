08c0d4502eab7642d4decb487bf1bb169b093c38
shandip6291-tech <shandip6291@gmail.com>
2026-06-14 21:07:20 +0530

    Update ready.js

--- Venus-HiveMind/events/ready.js
+++ Venus-HiveMind/events/ready.js
@@ -1,1 +1,20 @@
+module.exports = {
+  name: "ready",
+  once: true,
 
+  async execute(client) {
+    console.log(
+      `✅ ${client.user.tag} is online!`
+    );
+
+    client.user.setPresence({
+      activities: [
+        {
+          name: "Venus HiveMind",
+          type: 3
+        }
+      ],
+      status: "online"
+    });
+  }
+};
