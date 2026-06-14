4f10ba48b8c398479443193bda3f33b89a17aedd
shandip6291-tech <shandip6291@gmail.com>
2026-06-14 20:49:18 +0530

    Update interactioncreate.js

--- Venus-HiveMind/events/interactioncreate.js
+++ Venus-HiveMind/events/interactioncreate.js
@@ -1,1 +1,37 @@
+const ticketButtons = require("./tickets/ticketButtons");
+const ticketDropdowns = require("./tickets/ticketDropdowns");
+const ticketModals = require("./tickets/ticketModals");
 
+module.exports = {
+  name: "interactionCreate",
+
+  async execute(interaction) {
+
+    if (interaction.isButton()) {
+      return ticketButtons(interaction);
+    }
+
+    if (interaction.isStringSelectMenu()) {
+      return ticketDropdowns(interaction);
+    }
+
+    if (interaction.isModalSubmit()) {
+      return ticketModals(interaction);
+    }
+
+    if (!interaction.isChatInputCommand()) return;
+
+    const command =
+      interaction.client.commands.get(
+        interaction.commandName
+      );
+
+    if (!command) return;
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
