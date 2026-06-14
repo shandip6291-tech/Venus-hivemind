9c8bcd81fbb7b0c6497f94b2908e0fae00119796
shandip6291-tech <shandip6291@gmail.com>
2026-06-14 18:31:00 +0530

    Update timeout.js

--- Venus-HiveMind/commands/admin/timeout.js
+++ Venus-HiveMind/commands/admin/timeout.js
@@ -1,1 +1,41 @@
+const {
+  SlashCommandBuilder,
+  PermissionFlagsBits
+} = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("timeout")
+    .setDescription("Timeout a member")
+    .addUserOption(option =>
+      option
+        .setName("user")
+        .setDescription("User")
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName("minutes")
+        .setDescription("Minutes")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(
+      PermissionFlagsBits.ModerateMembers
+    ),
+
+  async execute(interaction) {
+    const member =
+      interaction.options.getMember("user");
+
+    const minutes =
+      interaction.options.getInteger("minutes");
+
+    await member.timeout(
+      minutes * 60 * 1000
+    );
+
+    await interaction.reply(
+      `⏳ ${member.user.tag} timed out for ${minutes} minutes`
+    );
+  }
+};
