1ce8bca94effd1174cb6f1c1778af551779de69b
shandip6291-tech <shandip6291@gmail.com>
2026-06-14 18:24:45 +0530

    Update ban.js

--- Venus-HiveMind/commands/admin/ban.js
+++ Venus-HiveMind/commands/admin/ban.js
@@ -1,1 +1,39 @@
+const {
+  SlashCommandBuilder,
+  PermissionFlagsBits
+} = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ban")
+    .setDescription("Ban a member")
+    .addUserOption(option =>
+      option
+        .setName("user")
+        .setDescription("User to ban")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(
+      PermissionFlagsBits.BanMembers
+    ),
+
+  async execute(interaction) {
+    const user =
+      interaction.options.getUser("user");
+
+    const member =
+      interaction.guild.members.cache.get(user.id);
+
+    if (!member)
+      return interaction.reply({
+        content: "User not found.",
+        ephemeral: true
+      });
+
+    await member.ban();
+
+    interaction.reply(
+      `🔨 ${user.tag} has been banned.`
+    );
+  }
+};
