8749e33d2d9404c991e42ee012eccb4421a07c31
shandip6291-tech <shandip6291@gmail.com>
2026-06-14 18:25:20 +0530

    Update kick.js

--- Venus-HiveMind/commands/admin/kick.js
+++ Venus-HiveMind/commands/admin/kick.js
@@ -1,1 +1,39 @@
+const {
+  SlashCommandBuilder,
+  PermissionFlagsBits
+} = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("kick")
+    .setDescription("Kick a member")
+    .addUserOption(option =>
+      option
+        .setName("user")
+        .setDescription("User to kick")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(
+      PermissionFlagsBits.KickMembers
+    ),
+
+  async execute(interaction) {
+    const user =
+      interaction.options.getUser("user");
+
+    const member =
+      interaction.guild.members.cache.get(user.id);
+
+    if (!member)
+      return interaction.reply({
+        content: "User not found.",
+        ephemeral: true
+      });
+
+    await member.kick();
+
+    interaction.reply(
+      `👢 ${user.tag} has been kicked.`
+    );
+  }
+};
