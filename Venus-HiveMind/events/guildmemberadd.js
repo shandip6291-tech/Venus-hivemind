fabac1138e78a6f9cb88c447f1d4d8439f738033
shandip6291-tech <shandip6291@gmail.com>
2026-06-14 21:09:40 +0530

    Update guildmemberadd.js

--- Venus-HiveMind/events/guildmemberadd.js
+++ Venus-HiveMind/events/guildmemberadd.js
@@ -1,1 +1,46 @@
+const {
+  EmbedBuilder
+} = require("discord.js");
 
+module.exports = {
+  name: "guildMemberAdd",
+
+  async execute(member) {
+
+    const channel =
+      member.guild.channels.cache.find(
+        c => c.name === "welcome"
+      );
+
+    if (!channel) return;
+
+    const embed =
+      new EmbedBuilder()
+        .setTitle(
+          "🎉 Member Joined"
+        )
+        .setDescription(
+          `Welcome ${member} to **${member.guild.name}**`
+        )
+        .addFields(
+          {
+            name: "User",
+            value:
+              member.user.tag
+          },
+          {
+            name: "Member Count",
+            value:
+              `${member.guild.memberCount}`
+          }
+        )
+        .setThumbnail(
+          member.user.displayAvatarURL()
+        );
+
+    channel.send({
+      embeds: [embed]
+    });
+
+  }
+};
