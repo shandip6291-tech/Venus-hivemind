cc73391cc66b5d6b72ae970e94f530e4a6e5164d
shandip6291-tech <shandip6291@gmail.com>
2026-06-14 21:10:54 +0530

    Update ticketlogs.js

--- Venus-HiveMind/events/tickets/ticketlogs.js
+++ Venus-HiveMind/events/tickets/ticketlogs.js
@@ -1,1 +1,42 @@
+const {
+  EmbedBuilder
+} = require("discord.js");
 
+module.exports = async (
+  guild,
+  action,
+  user,
+  channel
+) => {
+
+  const logChannel =
+    guild.channels.cache.find(
+      c => c.name === "ticket-logs"
+    );
+
+  if (!logChannel) return;
+
+  const embed =
+    new EmbedBuilder()
+      .setTitle("🎫 Ticket Log")
+      .addFields(
+        {
+          name: "Action",
+          value: action
+        },
+        {
+          name: "User",
+          value: `${user}`
+        },
+        {
+          name: "Channel",
+          value: `${channel}`
+        }
+      )
+      .setTimestamp();
+
+  await logChannel.send({
+    embeds: [embed]
+  });
+
+};
