480be7b925fdf2993dbd658ebceefe54173a3263
shandip6291-tech <shandip6291@gmail.com>
2026-06-14 21:09:55 +0530

    Update guildmemberremove.js

--- Venus-HiveMind/events/guildmemberremove.js
+++ Venus-HiveMind/events/guildmemberremove.js
@@ -1,1 +1,41 @@
+const {
+  EmbedBuilder
+} = require("discord.js");
 
+module.exports = {
+  name: "guildMemberRemove",
+
+  async execute(member) {
+
+    const channel =
+      member.guild.channels.cache.find(
+        c => c.name === "goodbye"
+      );
+
+    if (!channel) return;
+
+    const embed =
+      new EmbedBuilder()
+        .setTitle(
+          "👋 Member Left"
+        )
+        .setDescription(
+          `${member.user.tag} left the server.`
+        )
+        .addFields(
+          {
+            name: "Members Remaining",
+            value:
+              `${member.guild.memberCount}`
+          }
+        )
+        .setThumbnail(
+          member.user.displayAvatarURL()
+        );
+
+    channel.send({
+      embeds: [embed]
+    });
+
+  }
+};
