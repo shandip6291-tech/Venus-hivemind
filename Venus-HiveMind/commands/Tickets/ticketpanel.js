6c1bf1895e991e25ad95e227bb228aea7a9457a4
shandip6291-tech <shandip6291@gmail.com>
2026-06-14 20:50:19 +0530

    Update ticketpanel.js

--- Venus-HiveMind/commands/Tickets/ticketpanel.js
+++ Venus-HiveMind/commands/Tickets/ticketpanel.js
@@ -1,1 +1,40 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle
+} = require("discord.js");
 
+module.exports = {
+
+  data: new SlashCommandBuilder()
+    .setName("ticketpanel")
+    .setDescription("Send ticket panel"),
+
+  async execute(interaction) {
+
+    const embed =
+      new EmbedBuilder()
+        .setTitle("🎫 Venus Support Center")
+        .setDescription(
+          "Click the button below to create a support ticket."
+        );
+
+    const row =
+      new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+            .setCustomId("open_ticket")
+            .setLabel("Open Ticket")
+            .setEmoji("🎫")
+            .setStyle(ButtonStyle.Primary)
+        );
+
+    await interaction.reply({
+      embeds: [embed],
+      components: [row]
+    });
+
+  }
+};
